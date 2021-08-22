1cfd7bd0d6652cdba050f10e87efb51e645b8171
Moha-Mahdhour <hnood190channel@gmail.com>
2021-08-22 14:03:52 +0400

    Updated README file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-#Random Messages Generator
+# General Info
+- This is a script that generates a random message each time it runs.
+# Requirements
+- javascript
+- node 
+# Setup
+    To run this script install it locally.
+
+```
+$ cd ../message-gen
+$ node script.js
+```
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// an objet that contains all the message elements.
 const messageElements = {
     firstWord: ['you are', 'he is', 'she is'],
     secondWord: ['a strong', 'a fat', 'a crazy', 'a tall', 'a anxious', 'a big'],
@@ -7,11 +8,12 @@ const messageElements = {
 
 
 const randomMessage = obj => {
+    //choosing random words from each property in the objects
     let word1 = obj.firstWord[Math.floor(Math.random() * obj.firstWord.length)];
     let word2 = obj.secondWord[Math.floor(Math.random()* obj.secondWord.length)];
     let word3 = obj.lastWord[Math.floor(Math.random()* obj.lastWord.length)];
+    //linking everything to be in a sentence.
     console.log('Knock-Knock');
-
     console.log(`${word1} ${word2} ${word3}`);
 
 };
